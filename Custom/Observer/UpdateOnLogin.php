0e27ecd1aec59d9b49d389b4afdff18508580c9d
Tejas PANDYA <160587142+iamtejaspandya@users.noreply.github.com>
2024-03-07 17:57:32 +0530

    Add files via upload

---
+++ Custom/Observer/UpdateOnLogin.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tejas\Custom\Observer;
+
+use Magento\Framework\Event\Observer;
+use Magento\Framework\Event\ObserverInterface;
+use Magento\Customer\Api\CustomerRepositoryInterface;
+use Psr\Log\LoggerInterface;
+
+class UpdateOnLogin implements ObserverInterface
+{
+    protected $customerRepository;
+    protected $logger;
+
+    public function __construct(
+        CustomerRepositoryInterface $customerRepository,
+        LoggerInterface $logger
+    ) {
+        $this->customerRepository = $customerRepository;
+        $this->logger = $logger;
+    }
+
+    public function execute(Observer $observer)
+    {
+        try {
+            $customer = $observer->getData('customer');
+            $customerId = $customer->getId();
+            
+            $customerDataObject = $this->customerRepository->getById($customerId);
+            
+            $customerDataObject->setCustomAttribute('custom_new_attribute', 'login');
+            
+            $this->customerRepository->save($customerDataObject);
+        } catch (\Exception $e) {
+            $this->logger->error($e->getMessage());
+        }
+    }
+}
